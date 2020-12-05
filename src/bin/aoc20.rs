9b1ea5a304b561f8a8ca127850633c5d9ebc98b4
Cassandra McCord <cassandra.l.mccord@gmail.com>
2020-12-05 13:49:54 +0200

    updated all days run

--- src/bin/aoc20.rs
+++ src/bin/aoc20.rs
@@ -5,4 +5,26 @@ fn main() {
     day2::Day::new().run_day();
     day3::Day::new().run_day();
     day4::Day::new().run_day();
+    day5::Day::new().run_day();
+    day6::Day::new().run_day();
+    day7::Day::new().run_day();
+    day8::Day::new().run_day();
+    day9::Day::new().run_day();
+    day10::Day::new().run_day();
+    day11::Day::new().run_day();
+    day12::Day::new().run_day();
+    day13::Day::new().run_day();
+    day14::Day::new().run_day();
+    day15::Day::new().run_day();
+    day16::Day::new().run_day();
+    day17::Day::new().run_day();
+    day18::Day::new().run_day();
+    day19::Day::new().run_day();
+    day20::Day::new().run_day();
+    day21::Day::new().run_day();
+    day22::Day::new().run_day();
+    day23::Day::new().run_day();
+    day24::Day::new().run_day();
+    day25::Day::new().run_day();
+    
 }
