2b536890e2591eb6efcbf40fd199920ac844dfe4
Cassandra McCord <cassandra.l.mccord@gmail.com>
2020-12-13 13:11:37 +0200

    day 12,13

--- src/days/day12.rs
+++ src/days/day12.rs
@@ -1,11 +1,106 @@
-use std::convert::Infallible;
 use crate::ParsingError;
-use std::str::FromStr;
 use itertools::Itertools;
+use nalgebra::Vector2;
+use std::convert::Infallible;
+use std::str::FromStr;
 
 use crate::DayTrait;
-type Input = u64;
-type Output = u64;
+type Input = Action;
+type Output = i64;
+
+type Vec2 = Vector2<i64>;
+
+#[derive(Debug, Clone)]
+pub enum Dir {
+    North,
+    South,
+    East,
+    West,
+    Left,
+    Right,
+    Forward,
+}
+impl FromStr for Dir {
+    type Err = Infallible;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(match s {
+            "N" => Self::North,
+            "S" => Self::South,
+            "E" => Self::East,
+            "W" => Self::West,
+            "L" => Self::Left,
+            "R" => Self::Right,
+            "F" | _ => Self::Forward,
+        })
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Action {
+    dir: Dir,
+    amount: i64,
+}
+
+impl FromStr for Action {
+    type Err = ParsingError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Action {
+            dir: s[0..1].parse()?,
+            amount: s[1..].parse()?,
+        })
+    }
+}
+
+impl Action {
+    fn go(&self, heading: Vec2, pos: Vec2) -> (Vec2, Vec2) {
+        match self.dir {
+            Dir::North => (heading, pos + self.amount * Vec2::new(0, 1)),
+            Dir::South => (heading, pos + self.amount * Vec2::new(0, -1)),
+            Dir::East => (heading, pos + self.amount * Vec2::new(1, 0)),
+            Dir::West => (heading, pos + self.amount * Vec2::new(-1, 0)),
+            Dir::Left => match &self.amount {
+                90 => (Vec2::new(-heading[1], heading[0]), pos),
+                180 => (-heading, pos),
+                270 => (Vec2::new(heading[1], -heading[0]), pos),
+                _ => panic!(),
+            },
+            Dir::Right => match &self.amount {
+                90 => (Vec2::new(heading[1], -heading[0]), pos),
+                180 => (-heading, pos),
+                270 => (Vec2::new(-heading[1], heading[0]), pos),
+                _ => panic!(),
+            },
+            Dir::Forward => (heading, pos + heading * self.amount),
+        }
+    }
+    fn go2(&self, pos: Vec2, waypoint: Vec2) -> (Vec2, Vec2) {
+        let waypoint_off = waypoint - pos;
+        match self.dir {
+            Dir::North => (pos, waypoint + self.amount * Vec2::new(0, 1)),
+            Dir::South => (pos, waypoint + self.amount * Vec2::new(0, -1)),
+            Dir::East => (pos, waypoint + self.amount * Vec2::new(1, 0)),
+            Dir::West => (pos, waypoint + self.amount * Vec2::new(-1, 0)),
+            Dir::Left => match &self.amount {
+                90 => (pos, pos + Vec2::new(-waypoint_off[1], waypoint_off[0])),
+                180 => (pos, pos - waypoint_off),
+                270 => (pos, pos + Vec2::new(waypoint_off[1], -waypoint_off[0])),
+                _ => panic!(),
+            },
+            Dir::Right => match &self.amount {
+                270 => (pos, pos + Vec2::new(-waypoint_off[1], waypoint_off[0])),
+                180 => (pos, pos - waypoint_off),
+                90 => (pos, pos + Vec2::new(waypoint_off[1], -waypoint_off[0])),
+                _ => panic!(),
+            },
+            Dir::Forward => (
+                pos + waypoint_off * self.amount,
+                pos + waypoint_off * (self.amount + 1),
+            ),
+        }
+    }
+}
 
 #[derive(Default)]
 pub struct Day {}
@@ -15,18 +110,34 @@ impl DayTrait<Input, Output> for Day {
     }
 
     fn part1(&self, input: Vec<Input>) -> Output {
-        0
+        let heading: Vec2 = Vec2::new(1, 0);
+        let pos: Vec2 = Vec2::new(0, 0);
+        let ans = input
+            .into_iter()
+            .fold((heading, pos), |p, c| c.go(p.0, p.1))
+            .1
+            .abs()
+            .sum();
+        ans
     }
 
     fn part2(&self, input: Vec<Input>) -> Output {
-        0
+        let pos: Vec2 = Vec2::new(0, 0);
+        let waypoint: Vec2 = Vec2::new(10, 1);
+        let ans = input
+            .into_iter()
+            .fold((pos, waypoint), |p, c| c.go2(p.0, p.1))
+            .0
+            .abs()
+            .sum();
+        ans
     }
 
     fn part1_answer(&self) -> Output {
-        0
+        1956
     }
 
     fn part2_answer(&self) -> Output {
-        0
+        126797
     }
 }
--- src/days/day13.rs
+++ src/days/day13.rs
@@ -1,11 +1,61 @@
-use std::convert::Infallible;
 use crate::ParsingError;
-use std::str::FromStr;
 use itertools::Itertools;
+use std::convert::Infallible;
+use std::str::FromStr;
 
 use crate::DayTrait;
-type Input = u64;
-type Output = u64;
+type Input = Schedule;
+type Output = i128;
+
+#[derive(Debug, Clone)]
+pub struct Schedule {
+    now: i128,
+    busses: Vec<Option<i128>>,
+}
+
+impl FromStr for Schedule {
+    type Err = ParsingError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut lines = s.lines();
+        let now = lines.next().unwrap().parse()?;
+        let busses = lines
+            .next()
+            .unwrap()
+            .split(",")
+            .map(|x| x.parse::<i128>().ok())
+            .collect_vec();
+
+        Ok(Schedule { now, busses })
+    }
+}
+
+fn bez_pair(a: i128, b: i128) -> (i128, i128) {
+    let mut r = (a, b);
+    let mut s = (1, 0);
+    let mut t = (0, 1);
+
+    while r.1 != 0 {
+        let q = r.0 / r.1;
+        r = (r.1, r.0 - q * r.1);
+        s = (s.1, s.0 - q * s.1);
+        t = (t.1, t.0 - q * t.1);
+    }
+    //dbg!(&r, &s, &t);
+    (s.0, t.0)
+}
+
+fn pair(x: (i128, i128), y: (i128, i128)) -> i128 {
+    let a = (x.0, y.0);
+    let n = (x.1, y.1);
+    let m = bez_pair(n.0, n.1);
+    let mut v = (a.0 * m.1 * n.1 + a.1 * m.0 * n.0) % (n.1 * n.0);
+
+    if v < 0 {
+        v = v + n.1 * n.0
+    }
+    v
+}
 
 #[derive(Default)]
 pub struct Day {}
@@ -15,18 +65,74 @@ impl DayTrait<Input, Output> for Day {
     }
 
     fn part1(&self, input: Vec<Input>) -> Output {
-        0
+        let input = &input[0];
+        let next = input
+            .busses
+            .iter()
+            .flatten()
+            .map(|&x| {
+                (
+                    if input.now % x == 0 {
+                        input.now
+                    } else {
+                        ((input.now / x) + 1) * x
+                    },
+                    x,
+                )
+            })
+            .min_by_key(|x| x.0)
+            .unwrap();
+
+        (next.0 - input.now) * next.1
     }
 
-    fn part2(&self, input: Vec<Input>) -> Output {
-        0
+    fn part2(&self, mut input: Vec<Input>) -> Output {
+        input
+            .pop()
+            .unwrap()
+            .busses
+            .into_iter()
+            .enumerate()
+            .filter(|x| x.1.is_some())
+            .map(|x| (x.0 as i128, x.1.unwrap()))
+            .map(|x| (x.1 - x.0, x.1))
+            .fold1(|a, b| {
+                dbg!(&a, &b);
+                (pair(a, b), a.1 * b.1)
+            })
+            .unwrap()
+            .0
+        /*let a = bez_pair(input[0].1, input[1].1);
+        dbg!(&a);
+        let t = vec![(0, 3), (3, 4), (4, 5)];
+        let a = (pair(t[0], t[1]), t[0].1 * t[1].1);
+        dbg!(pair(a, t[2]));
+        0*/
+        /*let max = input.iter().max_by_key(|&x| x.1).unwrap();
+        (1i128..)
+            .find(|n| {
+                let start = n * max.1 - max.0;
+                /*if start == 3417 {
+                    dbg!(&start);
+                    dbg!(&input);
+                    dbg!(&input.iter().map(|x| start % x.1).collect_vec());
+                }*/
+                input.iter().all(|x| (start + x.0) % x.1 == 0)
+            })
+            .unwrap()
+            * max.1
+            - max.0*/
     }
 
     fn part1_answer(&self) -> Output {
-        0
+        2545
     }
 
     fn part2_answer(&self) -> Output {
-        0
+        266204454441577
+    }
+
+    fn read_input(&self) -> Result<Vec<Input>, ParsingError> {
+        crate::parse_list_delim(&format!("input/day{}.in", self.get_num()), "!")
     }
 }
