328344016f7d480b9cbfc0f9e6e8b3256ce60488
Cassandra McCord <cassandra.l.mccord@gmail.com>
2020-12-19 16:11:11 +0200

    day16-18

--- src/days/day16.rs
+++ src/days/day16.rs
@@ -1,11 +1,100 @@
-use std::convert::Infallible;
 use crate::ParsingError;
-use std::str::FromStr;
 use itertools::Itertools;
+use std::{collections::HashMap, str::FromStr};
+use std::{collections::HashSet, convert::Infallible};
 
 use crate::DayTrait;
-type Input = u64;
+type Input = InputData;
 type Output = u64;
+
+#[derive(Debug, Clone)]
+pub struct Range {
+    min: u64,
+    max: u64,
+}
+#[derive(Debug, Clone)]
+pub struct Field {
+    name: String,
+    ranges: Vec<Range>,
+}
+#[derive(Debug, Clone)]
+pub struct InputData {
+    fields: Vec<Field>,
+    mine: Vec<u64>,
+    other: Vec<Vec<u64>>,
+}
+impl FromStr for Range {
+    type Err = ParsingError;
+    fn from_str(s: &str) -> std::result::Result<Self, <Self as std::str::FromStr>::Err> {
+        let parts = s.split('-').collect_vec();
+        Ok(Range {
+            min: parts[0].parse()?,
+            max: parts[1].parse()?,
+        })
+    }
+}
+impl FromStr for Field {
+    type Err = ParsingError;
+    fn from_str(s: &str) -> Result<Self, ParsingError> {
+        let parts = s.split(": ").collect_vec();
+        let parts2 = parts[1]
+            .split(" or ")
+            .map(|x| x.parse::<Range>())
+            .try_collect()?;
+        Ok(Field {
+            name: parts[0].parse()?,
+            ranges: parts2,
+        })
+    }
+}
+
+impl Range {
+    fn validate(&self, data: u64) -> bool {
+        self.min <= data && self.max >= data
+    }
+}
+impl Field {
+    fn validate(&self, data: u64) -> bool {
+        self.ranges.iter().any(|x| x.validate(data))
+    }
+}
+impl InputData {
+    fn validate(&self, data: &[u64]) -> Option<u64> {
+        data.iter()
+            .find(|&x| self.fields.iter().all(|y| !y.validate(*x)))
+            .map(|&x| x)
+    }
+}
+impl FromStr for InputData {
+    type Err = ParsingError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut iter = s.lines();
+        let fields = iter
+            .by_ref()
+            .take_while(|&x| x != "")
+            .map(|x| x.parse::<Field>())
+            .try_collect()?;
+        iter.next();
+        let mine = iter
+            .next()
+            .unwrap()
+            .split(",")
+            .map(|x| x.parse())
+            .try_collect()?;
+        iter.next();
+        iter.next();
+        let other = iter
+            .map(|x| x.split(",").map(|y| y.parse()).try_collect())
+            .try_collect()?;
+
+        Ok(InputData {
+            fields,
+            mine,
+            other,
+        })
+    }
+}
 
 #[derive(Default)]
 pub struct Day {}
@@ -13,20 +102,66 @@ impl DayTrait<Input, Output> for Day {
     fn get_num(&self) -> usize {
         16
     }
-
+    fn read_input(&self) -> Result<Vec<Input>, <Input as FromStr>::Err> {
+        let file = std::fs::read_to_string(&format!("input/day{}.in", self.get_num())).unwrap();
+        Ok(vec![file.parse()?])
+    }
     fn part1(&self, input: Vec<Input>) -> Output {
-        0
+        let input = &input[0];
+        input
+            .other
+            .iter()
+            .map(|x| input.validate(x))
+            .flatten()
+            .sum()
     }
 
     fn part2(&self, input: Vec<Input>) -> Output {
-        0
+        let input = input.into_iter().next().unwrap();
+        let valid = input
+            .other
+            .iter()
+            .filter(|x| input.validate(x).is_none())
+            .collect_vec();
+        let mut remapped = vec![Vec::new(); input.mine.len()];
+        valid
+            .iter()
+            .for_each(|x| x.iter().enumerate().for_each(|y| remapped[y.0].push(*y.1)));
+        let fields = input.fields.clone();
+        let mut possible: Vec<HashSet<usize>> = vec![HashSet::new(); fields.len()];
+        fields.iter().enumerate().for_each(|(j, x)| {
+            for i in 0..fields.len() {
+                if remapped[i].iter().all(|y| x.validate(*y)) {
+                    possible[j].insert(i);
+                }
+            }
+        });
+        let res = possible
+            .iter()
+            .enumerate()
+            .sorted_by_key(|x| x.1.len())
+            .collect_vec();
+        let mut used = HashSet::new();
+        let mut mapping: HashMap<String, usize> = HashMap::new();
+        res.iter().for_each(|x| {
+            let intersection = x.1 - &used;
+            assert_eq!(intersection.len(), 1);
+            let value = intersection.into_iter().next().unwrap();
+            mapping.insert(fields[x.0].name.clone(), value);
+            used.insert(value);
+        });
+        mapping
+            .iter()
+            .filter(|x| x.0.starts_with("departure"))
+            .map(|x| input.mine[*x.1])
+            .product()
     }
 
     fn part1_answer(&self) -> Output {
-        0
+        22057
     }
 
     fn part2_answer(&self) -> Output {
-        0
+        1093427331937
     }
 }
--- src/days/day17.rs
+++ src/days/day17.rs
@@ -1,12 +1,77 @@
-use std::convert::Infallible;
 use crate::ParsingError;
-use std::str::FromStr;
 use itertools::Itertools;
+use nalgebra::Vector3;
+use nalgebra::Vector4;
+use std::{collections::HashMap, convert::Infallible};
+use std::{collections::HashSet, str::FromStr};
 
 use crate::DayTrait;
-type Input = u64;
-type Output = u64;
 
+type Vec3 = Vector3<i64>;
+type Vec4 = Vector4<i64>;
+
+#[derive(Debug, Clone)]
+pub struct Input(Vec<bool>);
+type Output = usize;
+
+impl FromStr for Input {
+    type Err = ParsingError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Input(s.chars().map(|x| x == '#').collect_vec()))
+    }
+}
+
+fn get_neighbors(v: &Vec3) -> Vec<Vec3> {
+    (-1..2)
+        .map(|x| {
+            (-1..2)
+                .map(move |y| (-1..2).map(move |z| v + Vec3::new(x, y, z)))
+                .flatten()
+        })
+        .flatten()
+        .collect_vec()
+}
+
+fn get_neighbors2(v: &Vec4) -> Vec<Vec4> {
+    (-1..2)
+        .map(|x| {
+            (-1..2)
+                .map(move |y| {
+                    (-1..2)
+                        .map(move |z| (-1..2).map(move |w| v + Vec4::new(x, y, z, w)))
+                        .flatten()
+                })
+                .flatten()
+        })
+        .flatten()
+        .collect_vec()
+}
+fn print_map(map: &HashMap<Vec3, bool>) {
+    let min_x = map.keys().map(|x| x[0]).min().unwrap();
+    let min_y = map.keys().map(|x| x[1]).min().unwrap();
+    let min_z = map.keys().map(|x| x[2]).min().unwrap();
+    let max_x = map.keys().map(|x| x[0]).max().unwrap();
+    let max_y = map.keys().map(|x| x[1]).max().unwrap();
+    let max_z = map.keys().map(|x| x[2]).max().unwrap();
+
+    for z in min_z..=max_z {
+        println!("Z={}", z);
+        for y in min_y..=max_y {
+            for x in min_x..=max_x {
+                print!(
+                    "{}",
+                    if *map.get(&Vec3::new(x, y, z)).unwrap_or(&false) {
+                        '#'
+                    } else {
+                        '.'
+                    }
+                )
+            }
+            println!("");
+        }
+    }
+}
 #[derive(Default)]
 pub struct Day {}
 impl DayTrait<Input, Output> for Day {
@@ -15,18 +80,133 @@ impl DayTrait<Input, Output> for Day {
     }
 
     fn part1(&self, input: Vec<Input>) -> Output {
-        0
+        let mut current: HashMap<Vec3, bool> = HashMap::new();
+        let mut next = current.clone();
+        input.iter().enumerate().for_each(|(y, v)| {
+            v.0.iter().enumerate().for_each(|(x, b)| {
+                current.insert(Vec3::new(x as i64, y as i64, 0), *b);
+            })
+        });
+
+        for _ in 0..6 {
+            //print_map(&current);
+            let cells: HashSet<Vec3> = current.keys().map(|x| get_neighbors(x)).flatten().collect();
+
+            cells.iter().for_each(|x| {
+                match (
+                    current.get(x).unwrap_or(&false),
+                    get_neighbors(x)
+                        .iter()
+                        .filter(|f| x != *f && *current.get(f).unwrap_or(&false))
+                        .count(),
+                ) {
+                    (true, 2) | (true, 3) | (false, 3) => {
+                        next.insert(x.clone(), true);
+                    }
+                    _ => (),
+                };
+            });
+            std::mem::swap(&mut current, &mut next);
+            next.clear();
+        }
+        current.values().filter(|&x| *x).count()
     }
 
     fn part2(&self, input: Vec<Input>) -> Output {
-        0
+        let amount = 6 * 2+3;
+        let ydim = input.len() + amount;
+        let xdim = input[0].0.len() + amount;
+        let zdim = amount;
+        let wdim = amount;
+        let xoff = 7;
+        let yoff = 7;
+        let zoff = 7;
+        let woff = 7;
+        let mut current2 = vec![vec![vec![vec![false; wdim]; zdim]; ydim]; xdim];
+
+        //let mut current: HashMap<Vec4, bool> = HashMap::new();
+
+        //let mut next = current.clone();
+        let mut next2 = current2.clone();
+        input.iter().enumerate().for_each(|(y, v)| {
+            v.0.iter().enumerate().for_each(|(x, b)| {
+                //current.insert(Vec4::new(x as i64, y as i64, 0, 0), *b);
+                current2[x + xoff][y + yoff][zoff][woff] = *b;
+            })
+        });
+
+        for _ in 0..6 {
+            //print_map(&current);
+            /*let cells: HashSet<Vec4> = current
+                .keys()
+                .map(|x| get_neighbors2(x))
+                .flatten()
+                .collect();
+
+            cells.iter().for_each(|x| {
+                match (
+                    current.get(x).unwrap_or(&false),
+                    get_neighbors2(x)
+                        .iter()
+                        .filter(|f| x != *f && *current.get(f).unwrap_or(&false))
+                        .count(),
+                ) {
+                    (true, 2) | (true, 3) | (false, 3) => {
+                        next.insert(x.clone(), true);
+                    }
+                    _ => (),
+                };
+            });*/
+
+            for x in 1..xdim - 1 {
+                for y in 1..ydim - 1 {
+                    for z in 1..zdim - 1 {
+                        for w in 1..wdim - 1 {
+                            let mut count = 0;
+                            let current = current2[x][y][z][w];
+                            for xx in -1..2i64 {
+                                for yy in -1..2i64 {
+                                    for zz in -1..2i64 {
+                                        for ww in -1..2i64 {
+                                            if xx == 0 && yy == 0 && zz == 0 && ww == 0 {
+                                                continue;
+                                            }
+                                            if current2[(x as i64 + xx) as usize]
+                                                [(y as i64 + yy) as usize]
+                                                [(z as i64 + zz) as usize]
+                                                [(w as i64 + ww) as usize]
+                                            {
+                                                count += 1;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                            next2[x][y][z][w] = count == 3 || (current && count == 2)
+                        }
+                    }
+                }
+            }
+
+            //std::mem::swap(&mut current, &mut next);
+            std::mem::swap(&mut current2, &mut next2);
+            //next.clear();
+        }
+        //current.values().filter(|&x| *x).count()
+        current2
+            .iter()
+            .flatten()
+            .flatten()
+            .flatten()
+            .filter(|&x| *x)
+            .count()
     }
 
     fn part1_answer(&self) -> Output {
-        0
+        273
     }
 
     fn part2_answer(&self) -> Output {
-        0
+        1504
     }
 }
